641df2865f59b52c000a057770fb9e712e66ac08
Giovanny472 <egmn472@yandex.com>
2021-01-17 22:02:19 +0300

    создание проекта

---
+++ app/src/main/java/ru/giovanny/gfilms/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.giovanny.gfilms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
